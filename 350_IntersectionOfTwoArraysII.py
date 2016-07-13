d2aa6ec72ee7863620d1a2f2b424f782b5578c85
nakagawahiroshi <nakagawa@goga.co.jp>
2016-07-13 17:38:12 +0900

    350

---
+++ 350_IntersectionOfTwoArraysII.py
@@ -0,0 +1,36 @@
+"""
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+Note:
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+"""
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort()
+        nums2.sort()
+        result = []
+        m, n = len(nums1), len(nums2)
+        i, j = 0,0
+        while i < m and j < n:
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+        return result
+
+s = Solution()
+print s.intersect([1, 2, 2, 1], [2, 2])

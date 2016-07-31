afe29b6fd5c626cfa2a323a0784f78ed9ed2957a
goga-nakagawa <nakagawa@goga-analysis.co.jp>
2016-08-01 08:07:24 +0900

    added

--- 189_RotateArray.py
+++ 189_RotateArray.py
@@ -35,6 +35,18 @@ class Solution(object):
                 nums.append(tmp)
             p, q = p + 1, q + 1
 
+
+    def rotate3(self, nums, k):
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
+
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+
+
 arr = [1,2,3,4,5,6,7]
 Solution().rotate(arr, 3)
 print arr
@@ -46,3 +58,7 @@ print arr1
 arr2 = [1,2,3,4,5,6,7]
 Solution().rotate2(arr2, 3)
 print arr2
+
+arr3 = [1,2,3,4,5,6,7]
+Solution().rotate3(arr3, 3)
+print arr3

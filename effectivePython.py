d79a6b9168a8cddd03fe43f642158836fb6cfb96
goga-nakagawa <nakagawa@goga-analysis.co.jp>
2016-11-29 09:49:33 +0900

    comb implemented

--- effectivePython.py
+++ effectivePython.py
@@ -1,12 +1,26 @@
-
-
-
-# memos
-def fib(n, memo={1:1, 2:1}):
-    if n in memo.keys():
-        return memo[n]
+def comb(arr, k, selected=[]):
+    if k == 0:
+        print selected
+    elif len(arr) == k:
+        print selected + arr[:k + 1]
     else:
-        memo[n] = fib(n - 2, memo) + fib(n - 1, memo)
-        return memo[n]
+        comb(arr[1:], k - 1, selected + [arr[0]])
+        comb(arr[1:], k, selected)
 
-print fib(10)
+# comb([1,2,3,4,5], 3)
+
+
+
+def comb1(arr, k):
+    if k == 0:
+        yield []
+    elif len(arr) == k:
+        yield arr[:k + 1]
+    else:
+        for c in comb1(arr[1:], k - 1):
+            yield c + arr[1:]
+        for c in comb1(arr[1:], k):
+            yield arr[1:]
+
+for c in comb1([1,2,3,4,5], 3):
+    print c

24d28d7496b9346c94da64fbc68dd2880029b1ad
goga-nakagawa <nakagawa@goga-analysis.co.jp>
2016-11-29 10:52:44 +0900

    added

--- effectivePython.py
+++ effectivePython.py
@@ -1,7 +1,7 @@
 
 
 
-# memos
+# recursive and memos 
 def fib(n, memo={1:1, 2:1}):
     if n in memo.keys():
         return memo[n]
@@ -10,3 +10,21 @@ def fib(n, memo={1:1, 2:1}):
         return memo[n]
 
 print fib(10)
+
+def pow(n, memo={0:1}):
+    if n in memo.keys():
+        return memo[n]
+    else:
+        memo[n] = n*pow(n - 1, memo)
+        return memo[n]
+
+print pow(6)
+
+def comb(n, k, memo):
+    if n in memo.keys():
+        return memo[n]
+    else:
+        memo[n] = comb(n, k-1, memo) * (n - k + 1) / k
+        return memo[n]
+
+print comb(6, 3, memo={(6, 0): 1, (6, 6): 1})

93b80d8843ef3abba20b80a9e7ac797c254a70bb
goga-nakagawa <nakagawa@goga-analysis.co.jp>
2016-11-30 23:16:43 +0900

    108 added

--- 7_ReverseInteger.py
+++ 7_ReverseInteger.py
@@ -1,3 +1,4 @@
+import re
 class Solution(object):
     def reverse(self, x):
         """
@@ -6,13 +7,11 @@ class Solution(object):
         -123
         -2147483648 - +2147483647
         """
-        i = len(str(x)) - 1
-        result = []
-        while i >= 0:
-            if i == 0 and str(x)[i] == '-':
-                result = ['-'] + result
-            else:
-                result.append(str(x)[i])
-            i -= 1
-        num = int(''.join(result))
-        return num if abs(num) < 2147483648 else 0
+        compiled = re.compile(r"^(\-?)(\d*)$")
+        operator, num = compiled.findall(str(x))[0]
+        rev = int(''.join(num[::-1]))
+        rev = 0 if abs(rev) < -2147483648 or abs(rev) > 2147483647 else rev
+        return int(operator + str(rev))
+
+s = Solution()
+print s.reverse(-126000)

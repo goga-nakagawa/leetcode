8bbdc14d03c0d9a4b42280904fae67b191c79270
Hiroshi Nakagawa <nakagawa@goga.co.jp>
2017-10-23 16:05:25 +0900

    moved python

--- python/effectivePython.py
+++ python/effectivePython.py
@@ -1,13 +1,26 @@
-# recursive
-
-def comb(n, m, a = []):
-    print a
-    if m == 0:
-        print a
-    elif n == m:
-        print range(1, m + 1) + a
+def comb(arr, k, selected=[]):
+    if k == 0:
+        print selected
+    elif len(arr) == k:
+        print selected + arr[:k + 1]
     else:
-        comb(n - 1, m, a)
-        comb(n - 1, m - 1, [n] + a) # nを選んだ
+        comb(arr[1:], k - 1, selected + [arr[0]])
+        comb(arr[1:], k, selected)
 
-comb(5, 3)
+# comb([1,2,3,4,5], 3)
+
+# recursive with generator
+def icomb(arr, k, selected=[]):
+    if k == 0:
+        yield selected
+    elif len(arr) == k:
+        yield selected + arr[:k + 1]
+    else:
+        for c in icomb(arr[1:], k - 1, selected + [arr[0]]):
+            yield c + arr[1:]
+        for c in icomb(arr[1:], k, selected):
+            yield arr[1:]
+
+
+for c in icomb([1,2,3,4,5], 3):
+    print c
